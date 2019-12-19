7f2ee393678d19b72473453cb5b7ddea3f9614a4
Winifred Frierson <winifred.dykes@hotmail.com>
2019-12-19 04:44:24 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
-
+def my_collect(array)
+  index = 0
+  modified_array = []
+  
+  
+  while index < array.length
+    modified_array << yield(array[index])
+    index += 1
+  end
+  
+  modified_array
+end
 
